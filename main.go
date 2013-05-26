626896482cb6b2c2075a9dad5036c95e0c59a26f
Danilo Cabello <danilo.cabello@gmail.com>
2013-05-26 09:52:38 -0400

    A working implementation now

--- main.go
+++ main.go
@@ -3,28 +3,105 @@ package main
 import (
 	"fmt"
     "bloom"
-    "random"
+    "math"
+    "text/tabwriter"
+    "os"
+    // "random"
 )
 
-func main() {
-    var keys []string = make([]string, 100)
-
-    var b bloom.Filter = bloom.New(250, 0.001)
-
-    rs := random.NewRS("abcdefghijklmnopqrstuvxz")
-    for i := 0; i < 100; i++ {
-        keys[i] = rs.NewRandomString(16)
-        b.Add([]byte(keys[i]))
+func Btoi(b bool) float64 {
+    if b {
+        return 1.0
     }
+    return 0.0
+}
 
-    fmt.Printf("%+v\n", b);
+func bench() {
+    var results map[string]float64
+    results = make(map[string]float64)
 
-    fmt.Printf("%v\n", b.Contains([]byte("guilherme")))
-    fmt.Printf("%v\n", b.Contains([]byte(keys[0])))
-    fmt.Printf("%v\n", b.Contains([]byte(keys[50])))
-    fmt.Printf("%v\n", b.Contains([]byte(keys[99])))
-    fmt.Printf("%v\n", b.Contains([]byte("adalberto")))
-    fmt.Printf("%v\n", b.Contains([]byte(keys[23])))
-    fmt.Printf("%v\n", b.Contains([]byte(keys[44])))
-    fmt.Printf("%v\n", b.Contains([]byte(keys[11])))
+    w := new(tabwriter.Writer)
+
+    // Format in tab-separated columns with a tab stop of 8.
+    w.Init(os.Stdout, 0, 8, 0, '\t', 0)
+    fmt.Fprintln(w, "N Elements\tError Prob\tM Bytes\tFalse Negative\tFalse Positive\tActual Prob\tTest")
+
+    // fmt.Fprintln(w, "a\tb\tc\td\t.")
+    // fmt.Fprintln(w, "123\t12345\t1234567\t123456789\t.")
+
+    // results[] = ['N Elements','Error Prob','M Bytes','False Negative','False Positive','Actual Prob'];
+    for i := 1.0; i < 6; i++ {
+        n := float64(math.Pow(10,i));
+        for p := 0.1; p > 0.0001; p /= 10 {
+            results["N Elements"] = n;
+            results["Error Prob"] = p;
+
+            fmt.Fprintf(w, "%v\t%v\t", n, p)
+
+            filter := bloom.New(int(n), p)
+
+            results["M Bytes"] = float64(filter.BitTotal >> 3)
+
+            fmt.Fprintf(w, "%v\t", float64(filter.BitTotal >> 3))
+
+            falseNegative := 0.0;
+            falsePositive := 0.0;
+
+            limit := n * 3;
+
+            for k := 0.0; k < limit; k+= 3.0 {
+                filter.Add([]byte(fmt.Sprintf("T%v", k)));
+            }
+
+            // fmt.Println(filter.BitArray)
+
+            samples := n * 9;
+            for k := 0.0; k < samples; k++ {
+                if int(k) % 3 == 0 && k < limit {
+                    falseNegative += Btoi(!filter.Contains([]byte(fmt.Sprintf("T%v", k))));
+                } else if k > limit {
+                    falsePositive += Btoi(filter.Contains([]byte(fmt.Sprintf("T%v", k))));
+                }
+            }
+
+            results["False Negative"] = falseNegative;
+            results["False Positive"] = falsePositive;
+            results["Actual Prob"] = falsePositive / samples;
+            if falsePositive / samples < p && falseNegative == 0 {
+                results["Test"] = 1
+            } else {
+                results["Test"] = 0
+            }
+
+            fmt.Fprintf(w, "%v\t%v\t%.4g\t%v\n", falseNegative, falsePositive, falsePositive / samples, falsePositive / samples < p && falseNegative == 0)
+
+            // fmt.Println(results);
+            // fmt.Println(filter.BitArray)
+        }
+    }
+    w.Flush()
+}
+
+func main() {
+    // var keys []string = make([]string, 100)
+
+    // var b bloom.Filter = bloom.New(250, 0.001)
+
+    // rs := random.NewRS("abcdefghijklmnopqrstuvxz")
+    // for i := 0; i < 100; i++ {
+    //     keys[i] = rs.NewRandomString(16)
+    //     b.Add([]byte(keys[i]))
+    // }
+
+    // fmt.Printf("%+v\n", b);
+
+    // fmt.Printf("%v\n", b.Contains([]byte("guilherme")))
+    // fmt.Printf("%v\n", b.Contains([]byte(keys[0])))
+    // fmt.Printf("%v\n", b.Contains([]byte(keys[50])))
+    // fmt.Printf("%v\n", b.Contains([]byte(keys[99])))
+    // fmt.Printf("%v\n", b.Contains([]byte("adalberto")))
+    // fmt.Printf("%v\n", b.Contains([]byte(keys[23])))
+    // fmt.Printf("%v\n", b.Contains([]byte(keys[44])))
+    // fmt.Printf("%v\n", b.Contains([]byte(keys[11])))
+    bench()
 }
--- src/bloom/bloom.go
+++ src/bloom/bloom.go
@@ -1,62 +1,53 @@
 package bloom
 
 import (
-	"fmt"
-	"crypto/sha256"
+	// "crypto/sha256"
 	"math"
-	"bytes"
-	"encoding/binary"
+	"math/big"
+	"mmh3"
 )
 
 type Filter struct {
-	bitCount      int
+	BitTotal      uint64
+	BitArray      []byte
 	hashFunctions int
 	chunkSize     int
-	bitArray      []byte
 }
 
 func New(capacity int, errorRate float64) Filter {
+	bitTotal := uint64(math.Pow(2, math.Ceil(math.Log2(-float64(capacity)*math.Log(errorRate)/math.Pow(math.Log(2), 2)))))
+	bitArray := make([]byte, bitTotal>>3)
 	hashFunctions := int(math.Floor(math.Log2(1.0 / errorRate)))
-	bitCount := int(math.Pow(2, math.Ceil(math.Log2(float64(-capacity)*math.Log(errorRate)/math.Pow(math.Log(2), 2)))))
-	addressBits := math.Log2(float64(bitCount))
-	chunkSize := int(math.Ceil(float64(addressBits / 8)))
-	bitArray := make([]byte, bitCount>>3)
+	// chunkSize := 32
+	chunkSize := int(math.Ceil(math.Log2(float64(bitTotal)) / 8))
 
 	return Filter{
-		bitCount,
+		bitTotal,
+		bitArray,
 		hashFunctions,
 		chunkSize,
-		bitArray,
 	}
 }
 
 func (f *Filter) Add(key []byte) {
-	hash := f.GenerateHashForKey(key)
+	hash := f.generateHashForKey(key)
 
 	for index := 0; index < f.hashFunctions; index++ {
-		unpackedBytes := hash[index*f.chunkSize : (index+1)*f.chunkSize]
-		unpackedBytesBuffer := bytes.NewBuffer(unpackedBytes)
-		unpackedValue, _ := binary.ReadUvarint(unpackedBytesBuffer)
-		subHash := unpackedValue % uint64(f.bitCount)
+		byteIndex, bitMask := f.findBitArrayIndexAndMask(hash, index)
 
-        fmt.Println("unpackedValue", unpackedValue % uint64(f.bitCount), "subHash", subHash)
-
-		word := subHash >> 3
-		f.bitArray[word] = f.bitArray[word] | 1<<uint(subHash%8)
+		f.BitArray[byteIndex] |= bitMask
 	}
+
+	// fmt.Printf("\n")
 }
 
 func (f *Filter) Contains(key []byte) bool {
-	hash := f.GenerateHashForKey(key)
+	hash := f.generateHashForKey(key)
 
 	for index := 0; index < f.hashFunctions; index++ {
-		unpackedBytes := hash[index*f.chunkSize : (index+1)*f.chunkSize]
-		unpackedBytesBuffer := bytes.NewBuffer(unpackedBytes)
-		unpackedValue, _ := binary.ReadUvarint(unpackedBytesBuffer)
-		subHash := unpackedValue % uint64(f.bitCount)
+		byteIndex, bitMask := f.findBitArrayIndexAndMask(hash, index)
 
-		word := subHash >> 3
-		if f.bitArray[word]&(1<<uint(subHash%8)) == 0 {
+		if f.BitArray[byteIndex]&bitMask == 0 {
 			return false
 		}
 	}
@@ -64,18 +55,46 @@ func (f *Filter) Contains(key []byte) bool {
 	return true
 }
 
-func (f *Filter) GenerateHashForKey(key []byte) []byte {
+// using murmurhash
+func (f *Filter) generateHashForKey(key []byte) []byte {
 	var completeHash []byte
-	var hash = sha256.New()
 
-	hash.Write(key)
-	completeHash = append(completeHash, hash.Sum(nil)...)
+	completeHash = mmh3.Hash128(key)
 
 	for f.chunkSize*f.hashFunctions > len(completeHash) {
-		hash.Reset()
-		hash.Write(completeHash)
-		completeHash = append(completeHash, hash.Sum(nil)...)
+		completeHash = append(completeHash, mmh3.Hash128(completeHash)...)
 	}
 
 	return completeHash
+}
+
+// // using sha256
+// func (f *Filter) generateHashForKey(key []byte) []byte {
+// 	var completeHash []byte
+// 	var hash = sha256.New()
+
+// 	hash.Write(key)
+// 	completeHash = append(completeHash, hash.Sum(nil)...)
+
+// 	for f.chunkSize*f.hashFunctions > len(completeHash) {
+// 		hash.Reset()
+// 		hash.Write(completeHash)
+// 		completeHash = append(completeHash, hash.Sum(nil)...)
+// 	}
+
+// 	return completeHash
+// }
+
+func (f *Filter) findBitArrayIndexAndMask(hash []byte, index int) (bitArrayIndex uint64, mask byte) {
+ 	var subHashInt big.Int
+
+	subHashBytes := hash[index*f.chunkSize : (index+1)*f.chunkSize]
+	subHashInt.SetBytes(subHashBytes)
+	position := subHashInt.Uint64()
+
+    position %= f.BitTotal
+
+	bitArrayIndex = uint64(position >> 3)
+	mask = 1 << uint(position%8)
+	return
 }
