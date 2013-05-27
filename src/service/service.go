db00cd75086d8e86d6b6679e3f9d82623b1b2bce
Danilo Cabello <danilo.cabello@gmail.com>
2013-05-27 14:57:15 -0400

    Defining the service API

--- src/service/service.go
+++ src/service/service.go
@@ -5,6 +5,7 @@ import (
     "fmt"
     "net/http"
     "log"
+    "github.com/gorilla/mux"
 )
 
 var filter bloom.Filter = bloom.New(100,0.01)
@@ -30,9 +31,37 @@ func contains(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusNotFound)
 }
 
+func createFilter(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "createFilter%+v\n", mux.Vars(r))
+}
+
+func retrieveFilter(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "retrieveFilter %+v\n", mux.Vars(r))
+}
+
+func deleteFilter(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "deleteFilter %+v\n", mux.Vars(r))
+}
+
+func createEntry(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "createEntry %+v\n", mux.Vars(r))
+}
+
+func retrieveEntry(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "retrieveEntry %+v\n", mux.Vars(r))
+}
+
 func ListenAndServe(address string) {
-    http.HandleFunc("/add/", add)
-    http.HandleFunc("/contains/", contains)
+    r := mux.NewRouter()
+    s := r.PathPrefix("/v1/filters").Subrouter()
+
+    s.HandleFunc("", createFilter).Methods("POST")
+    s.HandleFunc("/{filterName}", retrieveFilter).Methods("GET")
+    s.HandleFunc("/{filterName}", deleteFilter).Methods("DELETE")
+    s.HandleFunc("/{filterName}/entries", createEntry).Methods("POST")
+    s.HandleFunc("/{filterName}/entries/{entryName}", retrieveEntry).Methods("GET")
+
+    http.Handle("/", r)
     log.Print("Server starting on ", address)
     http.ListenAndServe(address, nil)
 }
