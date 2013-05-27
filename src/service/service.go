00a380782fae9b124ba9d76ea8db683a5b1094b8
Danilo Cabello <danilo.cabello@gmail.com>
2013-05-27 16:00:01 -0400

    Start reading post values

--- src/service/service.go
+++ src/service/service.go
@@ -1,67 +1,68 @@
 package service
 
 import (
-    "bloom"
-    "fmt"
-    "net/http"
-    "log"
-    "github.com/gorilla/mux"
+	"bloom"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
 )
 
-var filter bloom.Filter = bloom.New(100,0.01)
+var filter bloom.Filter = bloom.New(100, 0.01)
 
 func add(w http.ResponseWriter, r *http.Request) {
-    key := r.URL.Path[len("/add/"):]
+	key := r.URL.Path[len("/add/"):]
 
-    filter.Add([]byte(key));
+	filter.Add([]byte(key))
 
-    w.WriteHeader(http.StatusCreated)
-    fmt.Fprintf(w, "/contains/%s", key)
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "/contains/%s", key)
 }
 
 func contains(w http.ResponseWriter, r *http.Request) {
-    key := r.URL.Path[len("/contains/"):]
+	key := r.URL.Path[len("/contains/"):]
 
-    if (filter.Contains([]byte(key))) {
-        w.WriteHeader(http.StatusOK)
+	if filter.Contains([]byte(key)) {
+		w.WriteHeader(http.StatusOK)
 
-        return
-    }
+		return
+	}
 
-    w.WriteHeader(http.StatusNotFound)
+	w.WriteHeader(http.StatusNotFound)
 }
 
 func createFilter(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "createFilter%+v\n", mux.Vars(r))
+	fmt.Fprintf(w, "createFilter%+v\n", mux.Vars(r))
+	fmt.Fprintf(w, "name:%+v, capacity:%+v, errorRate:%+v\n", r.FormValue("name"), r.FormValue("capacity"), r.FormValue("errorRate"))
 }
 
 func retrieveFilter(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "retrieveFilter %+v\n", mux.Vars(r))
+	fmt.Fprintf(w, "retrieveFilter %+v\n", mux.Vars(r))
 }
 
 func deleteFilter(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "deleteFilter %+v\n", mux.Vars(r))
+	fmt.Fprintf(w, "deleteFilter %+v\n", mux.Vars(r))
 }
 
 func createEntry(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "createEntry %+v\n", mux.Vars(r))
+	fmt.Fprintf(w, "createEntry %+v\n", mux.Vars(r))
 }
 
 func retrieveEntry(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "retrieveEntry %+v\n", mux.Vars(r))
+	fmt.Fprintf(w, "retrieveEntry %+v\n", mux.Vars(r))
 }
 
 func ListenAndServe(address string) {
-    r := mux.NewRouter()
-    s := r.PathPrefix("/v1/filters").Subrouter()
+	r := mux.NewRouter()
 
-    s.HandleFunc("", createFilter).Methods("POST")
-    s.HandleFunc("/{filterName}", retrieveFilter).Methods("GET")
-    s.HandleFunc("/{filterName}", deleteFilter).Methods("DELETE")
-    s.HandleFunc("/{filterName}/entries", createEntry).Methods("POST")
-    s.HandleFunc("/{filterName}/entries/{entryName}", retrieveEntry).Methods("GET")
+	r.HandleFunc("/v1/filters", createFilter).Methods("POST")
+	r.HandleFunc("/v1/filters/{filterName}", retrieveFilter).Methods("GET")
+	r.HandleFunc("/v1/filters/{filterName}", deleteFilter).Methods("DELETE")
+	r.HandleFunc("/v1/filters/{filterName}/entries", createEntry).Methods("POST")
+	r.HandleFunc("/v1/filters/{filterName}/entries/{entryName}", retrieveEntry).Methods("GET")
 
-    http.Handle("/", r)
-    log.Print("Server starting on ", address)
-    http.ListenAndServe(address, nil)
+	http.Handle("/", r)
+
+	log.Print("Server starting on ", address)
+	http.ListenAndServe(address, nil)
 }
